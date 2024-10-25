ddd91c3e236c23359dc119f9117c60c7468778fc
ChristianScarabelli <scachmabol@hotmail.it>
2024-10-25 15:46:05 +0200

    ADD: snack 2

--- app/script.js
+++ app/script.js
@@ -28,6 +28,7 @@ const bicycle4 = {  // Oggetto4
 
 const bicycles = [bicycle1, bicycle2, bicycle3, bicycle4]  // Array di oggetti
 
+// Stampo in console la bici con peso minore
 console.log(bicycles[2])
 
 
@@ -36,9 +37,57 @@ Snack2
 Creare un array di oggetti di squadre di calcio.
 Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Generare numeri random al posto degli 0 nelle proprietà “punti fatti" e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
+
+// Funzione per generare numeri casuali (da 1 a 100)
+function getRandomNumber() {
+    return Math.floor((Math.random() * 100) + 1)
+}
+
+const squad1 = {  // Oggetto1
+    name: 'fiorentina',
+    score: '0',
+    penalties: '0'
+}
+
+const squad2 = {  // Oggetto2
+    name: 'milan',
+    score: '0',
+    penalties: '0'
+}
+
+const squad3 = {  // Oggetto3
+    name: 'inter',
+    score: '0',
+    penalties: '0'
+}
+
+const squad4 = {  // Oggetto4
+    name: 'juventus',
+    score: '0',
+    penalties: '0'
+}
+
+const soccerSquads = [squad1, squad2, squad3, squad4]  // Array di oggetti
+
+// per ogni Squad dell'array soccerSquads, richiamo il valori di tutti gli oggetti contenuti nell'array,
+// tramite la funzione assegno i nuemri random
+for (let i = 0; i < soccerSquads.length; i++) {
+    soccerSquads[i].score = getRandomNumber()
+    soccerSquads[i].penalties = getRandomNumber()
+}
+
+// Creazione di un nuovo array con solo nome e falli subiti
+const nameAndPenalties = soccerSquads.map(squad => {
+    return { name: squad.name, penalties: squad.penalties }
+})
+
+// Stampo in console il nuovo array i cui elementi contengono solo nomi e falli subiti
+console.log(nameAndPenalties)
+
+
 
 /*
 Snack 3 (Bonus)
